3366929689310aaa78695d976294c52d8a38d398
doingb <hjyeapkfvb76@hotmail.com>
2025-01-04 18:45:17 +0800

    Add : monitor new user

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const TwitterClient = require('./twitter-client');
+const readline = require('readline');
+
+function askQuestion(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
 
 async function main() {
   const twitter = new TwitterClient();
@@ -11,18 +26,26 @@ async function main() {
     return;
   }
 
-  console.log('\n--- Example: Getting recent tweets from a user ---');
-  const userTweets = await twitter.getUserTweets('elonmusk', 5);
+  console.log('\n--- Getting tweets from a user ---');
+  const username = await askQuestion('请输入要查看推特的用户名 (不含@): ');
+  const tweetCount = await askQuestion('请输入要获取的推特数量 (默认5): ') || '5';
+  
+  const userTweets = await twitter.getUserTweets(username, parseInt(tweetCount));
   if (userTweets) {
+    console.log(`\n@${username} 的最新推特:`);
     userTweets.forEach((tweet, index) => {
       console.log(`${index + 1}. ${tweet.text}`);
       console.log(`   Posted: ${tweet.created_at}\n`);
     });
   }
 
-  console.log('\n--- Example: Searching for tweets ---');
-  const searchResults = await twitter.searchTweets('javascript', 5);
+  console.log('\n--- Searching for tweets ---');
+  const searchQuery = await askQuestion('请输入搜索关键词: ');
+  const searchCount = await askQuestion('请输入要获取的推特数量 (默认5): ') || '5';
+  
+  const searchResults = await twitter.searchTweets(searchQuery, parseInt(searchCount));
   if (searchResults) {
+    console.log(`\n关键词 "${searchQuery}" 的搜索结果:`);
     searchResults.forEach((tweet, index) => {
       console.log(`${index + 1}. ${tweet.text}`);
       console.log(`   Posted: ${tweet.created_at}\n`);
